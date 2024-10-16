2b92319d5756ab57faade623e0d2a068244fac38
Герман <65992547+German322VDK@users.noreply.github.com>
2024-10-16 23:33:24 +1000

    ViewModels base content

---
+++ Forum.Domain/ViewModels/Contents/Base/CommentViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Forum.Domain.ViewModels.Contents.Base
+{
+    /// <summary>
+    /// Represents a comment view model.
+    /// Inherits from <see cref="ContentViewModel"/>.
+    /// </summary>
+    public class CommentViewModel : ContentViewModel { }
+}
---
+++ Forum.Domain/ViewModels/Contents/Base/PostViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Forum.Domain.ViewModels.Contents.Base
+{
+    /// <summary>
+    /// Represents a post view model.
+    /// Inherits from <see cref="ContentViewModel"/>.
+    /// </summary>
+    public class PostViewModel : ContentViewModel
+    {
+        /// <summary>
+        /// Gets or sets the comments associated with the post.
+        /// </summary>
+        public IEnumerable<CommentViewModel> Comments { get; set; }
+    }
+}

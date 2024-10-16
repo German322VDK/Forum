11f07da8f18cf9e3f003c86f69be36b4af492d59
Герман <65992547+German322VDK@users.noreply.github.com>
2024-10-16 23:25:47 +1000

    Enitity Likes

---
+++ Forum.Domain/Entities/ForumEntities/Likes/CommentLikeEntity.cs
@@ -0,0 +1,19 @@
+﻿namespace Forum.Domain.Entities.ForumEntities.Likes
+{
+    /// <summary>
+    /// Represents a like for a specific comment.
+    /// Inherits from <see cref="LikeEntity"/>.
+    /// </summary>
+    public class CommentLikeEntity : LikeEntity
+    {
+        /// <summary>
+        /// Gets or sets the ID of the comment that was liked.
+        /// </summary>
+        public int CommentId { get; set; }
+
+        /// <summary>
+        /// Navigation property for the comment that was liked.
+        /// </summary>
+        public virtual CommentEntity Comment { get; set; }
+    }
+}

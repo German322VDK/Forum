6fa0a7a8dae4a5a4cbda3ba658a8fa6eee619eff
Герман <65992547+German322VDK@users.noreply.github.com>
2024-10-16 23:25:20 +1000

    Entitiy Identities

---
+++ Forum.Domain/Entities/Identity/RoleEntity.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Forum.Domain.Entities.Identity
+{
+    /// <summary>
+    /// Represents a role in the identity system.
+    /// Inherits from <see cref="IdentityRole{TKey}"/> where TKey is of type int.
+    /// </summary>
+    public class RoleEntity : IdentityRole<int>
+    {
+    }
+}
---
+++ Forum.Domain/Entities/Identity/UserStatus.cs
@@ -0,0 +1,23 @@
+﻿namespace Forum.Domain.Entities.Identity
+{
+    /// <summary>
+    /// Defines the different statuses a user can have in the system.
+    /// </summary>
+    public enum UserStatus
+    {
+        /// <summary>
+        /// User with administrative privileges.
+        /// </summary>
+        Admin = 1,
+
+        /// <summary>
+        /// Regular user with standard privileges.
+        /// </summary>
+        User = 2,
+
+        /// <summary>
+        /// User who has been banned from the system.
+        /// </summary>
+        Banned = 3
+    }
+}

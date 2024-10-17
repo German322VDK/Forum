fce016b8267de8ba2deccfdb3c9f25d07c387bc8
Герман <65992547+German322VDK@users.noreply.github.com>
2024-10-17 16:59:26 +1000

    db init

---
+++ Forum.Infrastructure/Initializers/IDbInitializer.cs
@@ -0,0 +1,14 @@
+﻿namespace Forum.Infrastructure.Initializers
+{
+    /// <summary>
+    /// Interface for initializing the database.
+    /// </summary>
+    public interface IDbInitializer
+    {
+        /// <summary>
+        /// Initializes the database by applying any necessary migrations and seeding initial data.
+        /// </summary>
+        /// <returns>A task that represents the asynchronous initialization operation.</returns>
+        public Task InitializeAsync();
+    }
+}
